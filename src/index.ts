2ef5c3e94cc4d52042a4e0c6d080c86c303f3361
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-08 01:18:19 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let numero: number = 
+
+if(numero === 0) {
+  console.log("El " + numero + " no es par ni impar");
+} if (numero % 2) {
+  console.log("El " + numero + " es impar");
+} else {
+  console.log("El " + numero + " es par");
+}
